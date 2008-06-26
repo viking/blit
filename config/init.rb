bcfec653743ac1f4f734e1426dc5c150207c5794
Jeremy Stephens <viking415@gmail.com>
2008-06-26 18:24:46 -0500

    Updates posts views

--- config/init.rb
+++ config/init.rb
@@ -57,7 +57,7 @@ Merb.push_path(:lib, Merb.root / "lib") # uses **/*.rb as path glob.
 # dependency "RedCloth", "> 3.0"
 # OR
 # dependencies "RedCloth" => "> 3.0", "ruby-aes-cext" => "= 1.0"
-dependencies "git", "merb-haml", "activesupport"
+dependencies "git", "merb-haml", "activesupport", "merb-assets", "merb_helpers"
 Merb::BootLoader.after_app_loads do
   # Add dependencies here that must load after the application loads:
 
